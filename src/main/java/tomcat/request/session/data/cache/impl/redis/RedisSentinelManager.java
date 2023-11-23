f9907c6e80dfa211a69294b2be033c4920002152
KHW <k72780462@gmail.com>
2023-11-23 14:39:16 +0900

    test

--- src/main/java/tomcat/request/session/data/cache/impl/redis/RedisSentinelManager.java
+++ src/main/java/tomcat/request/session/data/cache/impl/redis/RedisSentinelManager.java
@@ -10,12 +10,32 @@ class RedisSentinelManager extends RedisManager {
 
     private static final long FAILURE_WAIT_TIME = 2000L;
 
+    static RedisSentinelManager createInstanceBy(Set<String> nodes,
+                                                 String masterName,
+                                                 String username,
+                                                 String password,
+                                                 int database,
+                                                 int timeout,
+                                                 JedisPoolConfig poolConfig) {
+        boolean hasUsername = username != null && username.length() > 0;
+        if(hasUsername) {
+            return new RedisSentinelManager(nodes, masterName, username, password, database, timeout, poolConfig);
+        } else {
+            return new RedisSentinelManager(nodes, masterName, username, database, timeout, poolConfig);
+        }
+    }
+
     RedisSentinelManager(Set<String> nodes,
                          String masterName,
+                         String username,
                          String password,
                          int database,
                          int timeout,
                          JedisPoolConfig poolConfig) {
-        super(new JedisSentinelPool(masterName, nodes, poolConfig, timeout, password, database), FAILURE_WAIT_TIME);
+        super(new JedisSentinelPool(masterName, nodes, poolConfig, timeout, username, password, database), FAILURE_WAIT_TIME);
+    }
+
+    RedisSentinelManager(Set<String> nodes, String masterName, String password, int database, int timeout, JedisPoolConfig poolConfig) {
+        super(new JedisSentinelPool(masterName, nodes, poolConfig, timeout, password, database), 2000L);
     }
 }
